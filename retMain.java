377b1d4775a14b2622641eae3b5b413052011027
MotAtlas <25167914+MotAtlas@users.noreply.github.com>
2017-11-07 11:15:03 +0100

    Test

--- retMain.java
+++ retMain.java
@@ -2,16 +2,14 @@ import java.util.*;
 public class retMain{
 	public static void main(String[] args){
 		Scanner reader = new Scanner(System.in);
-		Rettangolo figura1, figura2;
-		System.out.println("Inserisci lato 1 e lato 2 del rettangolo 1");
-		figura1 = new Rettangolo(reader.nextDouble(), reader.nextDouble());
-		System.out.println("Inserisci lato 1 e lato 2 del rettangolo 2");
-		figura2 = new Rettangolo(reader.nextDouble(), reader.nextDouble());
-		System.out.println("Rettangolo 1:");
-		System.out.println("Area = " +figura1.areaCalc());
-		System.out.println("Perimetro = " +figura1.perimCalc());
-		System.out.println("Rettangolo 2:");
-		System.out.println("Area = " +figura2.areaCalc());
-		System.out.println("Perimetro = " +figura2.perimCalc());
+		Rettangolo[] ret = new Rettangolo[10];
+		for(int i = 0; i < 10; i ++){
+			System.out.println("Inserisci lato 1 e lato 2 del rettangolo "+i);
+			ret[i] = new Rettangolo(reader.nextDouble(), reader.nextDouble());
+			System.out.println("Rettangolo "+i+":");
+			System.out.println("Area = " +figura1.areaCalc());
+			System.out.println("Perimetro = " +figura1.perimCalc());
+		}
+		
 	}
 }
