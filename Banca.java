f8413b0209f959df29304f1414d966cbe8bce8ab
Andrea Grandi <MotAtlas@gmail.com>
2017-12-05 11:43:01 +0100

    ContoCorrente

--- Banca.java
+++ Banca.java
@@ -4,7 +4,7 @@ public class Banca{
 		Scanner reader = new Scanner(System.in);
 		ContoCorrente conto1 = null;
 		while(true){
-			System.out.println("a. Apri conto corrente\n b. Chiudi conto corrente\n c. Fai un prelievo.");
+			System.out.println(" a. Apri conto corrente\n b. Chiudi conto corrente\n c. Fai un prelievo.\n d. Fai un versamento");
 			String inpt = reader.next();
 			if(inpt.equals("a")){
 				System.out.println("Inserisci nome intestatario");
@@ -12,7 +12,8 @@ public class Banca{
 				String intestatario = reader.nextLine();
 				System.out.println("Inserisci saldo iniziale");
 				double saldo = reader.nextDouble();
-				conto1 = new ContoCorrente(saldo,intestatario);
+				conto1 = new ContoCorrente(saldo,intestatario,generateRandomChars("123456789",10));
+				System.out.println("Numero conto = "+conto1.getNConto());
 			}
 			else if(inpt.equals("b")){
 				conto1 = null;
@@ -21,7 +22,12 @@ public class Banca{
 				if(conto1 != null){
 					System.out.println("Quanto vuoi prelevare?");
 					double prel = reader.nextDouble();
-					conto1.preleva(prel);
+					if(conto1.preleva(prel)){
+						System.out.println("	Prelievo completato");
+					}
+					else{
+						System.out.println("	Prelievo non completato");
+					}
 				}
 			}
 			else if(inpt.equals("d")){
@@ -29,8 +35,18 @@ public class Banca{
 					System.out.println("Quanto vuoi depositare?");
 					double prel = reader.nextDouble();
 					conto1.versa(prel);
+					System.out.println("	Versamento completato");
 				}
 			}
 		}
+	}
+	public static String generateRandomChars(String candidateChars, int length) {
+	    StringBuilder sb = new StringBuilder();
+	    Random random = new Random();
+	    for (int i = 0; i < length; i++) {
+	        sb.append(candidateChars.charAt(random.nextInt(candidateChars.length())));
+	    }
+
+	    return sb.toString();
 	}
 }
