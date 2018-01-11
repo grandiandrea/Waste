9bb2b1a85e855a63727d7fa93e1bd31592f17a19
Andrea Grandi <MotAtlas@gmail.com>
2018-01-11 10:18:57 +0100

    changes

---
+++ ContoCorrenteLista/FileManager.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class FileManager{
+	Path buffer;
+	Path registro;
+	public FileManager(){
+		this.buffer = Paths.get("buffer.txt");
+	}
+	public Write(FileWrapper contenuto){
+		List<FileWrapper> lista = new ArrayList<>();
+		lista.add(contenuto);
+	}
+}
---
+++ ContoCorrenteLista/FileWrapper.java
@@ -0,0 +1,16 @@
+public class FileWrapper{
+	int nconto; 
+	String nome;
+	String cognome; 
+	String operazione; 
+	double amount; 
+	Date data;
+	public FileWrapper(int nconto, String nome,String cognome, String operazione, double amount, Date data){
+		this.nconto = nconto; 
+		this.nome = nome;
+		this.cognome = cognome; 
+		this.operazione = operazione; 
+		this.amount = amount; 
+		this.data = data;
+	}
+}
