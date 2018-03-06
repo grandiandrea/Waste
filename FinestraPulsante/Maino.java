fbdba70d54d334c9b53d71c6652fe8a7856da00a
Andrea Grandi <MotAtlas@gmail.com>
2018-03-06 11:45:49 +0100

    Finestraaa

---
+++ FinestraPulsante/Maino.java
@@ -0,0 +1,38 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+import javax.swing.BorderFactory;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+public class Maino{
+  public static void main(String[] args){
+    JFrame guiFrame = new JFrame();
+    guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		guiFrame.setTitle("Finestra con pulsante");
+		guiFrame.setSize(650,680);
+		guiFrame.setLocationRelativeTo(null);
+    JPanel pannello = new JPanel();
+    JButton bottone1 = new JButton("Bottone1");
+    JLabel etichetta1 = new JLabel("Etichetta");
+    bottone1.addActionListener(new ActionListener(){
+      @Override
+      public void actionPerformed(ActionEvent event){
+        etichetta1.setText("MA CAMBIA YOO");
+      }
+    });
+    pannello.add(bottone1);
+    pannello.add(etichetta1);
+    guiFrame.add(pannello);
+    guiFrame.setVisible(true);
+  }
+}
